636a3828a3e9a3cbfe2ec2548846a180b19cedac
BrunoLeon007 <brunob734@gmail.com>
2024-11-21 20:23:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ var btnDarkMode = document.querySelector("#darkmode")
 function darkmode(){
    
 body.classList.toggle("darkmode")
-
+if(btnDarkMode.textContent == "Ativar"){
+    btnDarkMode.textContent = "Desativar"
+}else{
+    btnDarkMode.textContent = "Ativar"
+}
 }
 
 btnDarkMode.addEventListener("click" , darkmode)
+
+
+
+
